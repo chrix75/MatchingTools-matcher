34bdcbf5e688bdb138b52fdc7fc650285dd9a5fb
Christian Sperandio <christian.sperandio@gmail.com>
2016-09-04 11:41:26 +0200

    Clean code.

--- src/test/groovy/matchingtools/matching/CompanyMatcherTest.groovy
+++ src/test/groovy/matchingtools/matching/CompanyMatcherTest.groovy
@@ -50,71 +50,71 @@ class CompanyMatcherTest extends GroovyTestCase {
     }
 
     void testMatch_1() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([ims], [aguesseau128], "", boulogne)
+        def record1 = new Record(1, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], "", boulogne)
 
         assertEquals(Match.MATCH, matcher.match(record1, record2))
     }
 
     void testMatch_2() {
-        def record1 = new Record([ims], [aguesseau122], "", boulogne)
-        def record2 = new Record([ims], [aguesseau128], "", boulogne)
+        def record1 = new Record(1, [ims], [aguesseau122], "", boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], "", boulogne)
 
         assertEquals(Match.MATCH, matcher.match(record1, record2))
     }
 
     void testMatch_3() {
-        def record1 = new Record([ims], [aguesseau137], "", boulogne)
-        def record2 = new Record([ims], [aguesseau128], "", boulogne)
+        def record1 = new Record(1, [ims], [aguesseau137], "", boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], "", boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_4() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([cegedim], [aguesseau128], "", boulogne)
+        def record1 = new Record(1, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(2, [cegedim], [aguesseau128], "", boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_5() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([ims], [aguesseau128, reine], "", boulogne)
+        def record1 = new Record(3, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(4, [ims], [aguesseau128, reine], "", boulogne)
 
         assertEquals(Match.MATCH, matcher.match(record1, record2))
     }
 
     void testMatch_6() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([ims], [reine], "", boulogne)
+        def record1 = new Record(5, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(6, [ims], [reine], "", boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_7() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([ims], [aguesseau128], "", paris)
+        def record1 = new Record(1, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], "", paris)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_8() {
-        def record1 = new Record([ims], [aguesseau128], siret1, boulogne)
-        def record2 = new Record([ims], [aguesseau128], siret2, boulogne)
+        def record1 = new Record(1, [ims], [aguesseau128], siret1, boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], siret2, boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_9() {
-        def record1 = new Record([ims], [reine], siret1, boulogne)
-        def record2 = new Record([ims], [aguesseau128], siret1, boulogne)
+        def record1 = new Record(1, [ims], [reine], siret1, boulogne)
+        def record2 = new Record(2, [ims], [aguesseau128], siret1, boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
 
     void testMatch_10() {
-        def record1 = new Record([ims], [aguesseau128], "", boulogne)
-        def record2 = new Record([imsCommunicationDirect], [aguesseau128], "", boulogne)
+        def record1 = new Record(1, [ims], [aguesseau128], "", boulogne)
+        def record2 = new Record(2, [imsCommunicationDirect], [aguesseau128], "", boulogne)
 
         assertEquals(Match.UNMATCH, matcher.match(record1, record2))
     }
